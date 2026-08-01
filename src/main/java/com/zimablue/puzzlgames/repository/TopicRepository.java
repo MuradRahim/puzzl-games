e6d6b4f31b8cea1ed21c2aacd7da078aeb3fe0a2
Raimzhanov <Raimzhanov@lanit.ru>
2026-08-01 23:54:46 +0300

    delete JDBC into Hibernate

--- src/main/java/com/zimablue/puzzlgames/repository/TopicRepository.java
+++ src/main/java/com/zimablue/puzzlgames/repository/TopicRepository.java
@@ -10,14 +10,4 @@ import java.util.Optional;
 public interface TopicRepository extends JpaRepository<Topic, Long> {
 
     Optional<Topic> findBySlug(String slug);
-
-    @Query(value = """
-        SELECT t.id, t.slug, t.title, t.description,
-                               COUNT(q.id) AS question_count
-                        FROM topics t
-                                 LEFT JOIN questions q ON q.topic_id = t.id
-                        GROUP BY t.id, t.slug, t.title, t.description
-                        ORDER BY t.id
-        """, nativeQuery = true)
-    Optional<Topic> findByIdWithQuestions(@Param("id") Long id);
 }
